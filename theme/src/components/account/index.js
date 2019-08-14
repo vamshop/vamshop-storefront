01ba17ca2a7fd462a42173228fa73e1b3421da17
vam <vamshop@gmail.com>
2019-08-14 23:02:45 +0300

    Корректирока входа в аккаунт

--- theme/src/components/account/index.js
+++ theme/src/components/account/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Lscache from 'lscache';
 import { themeSettings, text } from '../../lib/settings';
+import AuthHeader from '../../../../src/server/auth-header';
 import Account from './account';
 
 export default class AccountForm extends React.Component {
@@ -10,7 +11,7 @@ export default class AccountForm extends React.Component {
 	}
 
 	handlecustomerProperties = () => {
-		this.props.customerData({
+    	this.props.customerData({
 			token: Lscache.get('auth_data')
 		});
 	};
@@ -21,7 +22,7 @@ export default class AccountForm extends React.Component {
 			first_name: values.first_name,
 			last_name: values.last_name,
 			email: values.email,
-			password: values.password,
+			password: AuthHeader.encodeUserPassword(values.password),
 			token: Lscache.get('auth_data'),
 			shipping_address,
 			billing_address,
@@ -29,16 +30,17 @@ export default class AccountForm extends React.Component {
 		});
 
 		this.props.updateCart({
-			shipping_address,
-			billing_address,
+			shipping_address: shipping_address,
+			billing_address: billing_address,
 			payment_method_id: null,
 			shipping_method_id: null
 		});
-	};
+	}
 
 	render() {
+
 		const {
-			settings,
+      		settings,
 			customerProperties,
 			initialValues,
 			cartlayerBtnInitialized
@@ -49,46 +51,39 @@ export default class AccountForm extends React.Component {
 		if (Lscache.get('auth_data') === null && customerProperties === undefined) {
 			Lscache.flush();
 			return (
-				<Redirect
-					to={{
+			  <Redirect to={{
 						pathname: '/login'
-					}}
-				/>
+				}}/>
 			);
-		}
-		const cacheTimeStamp = localStorage.getItem(
-			'lscache-auth_data-cacheexpiration'
-		);
-		if (Number(cacheTimeStamp) <= Math.floor(new Date().getTime() / 1000)) {
-			Lscache.flush();
+		} else {
+			const cacheTimeStamp = localStorage.getItem('lscache-auth_data-cacheexpiration');
+			if (Number (cacheTimeStamp) <= Math.floor((new Date().getTime()) / 1000)) {
+				Lscache.flush();
+				return (
+					<Redirect to={{
+						pathname: '/login'
+					}}/>
+				)
+			}
+				
+			const {
+				checkoutInputClass = 'checkout-field',
+				checkoutButtonClass = 'checkout-button',
+				checkoutEditButtonClass = 'checkout-button-edit'
+			} = themeSettings;
+	
 			return (
-				<Redirect
-					to={{
-						pathname: '/login'
-					}}
+				<Account
+					inputClassName={checkoutInputClass}
+					buttonClassName={checkoutButtonClass}
+					editButtonClassName={checkoutEditButtonClass}				
+					settings={settings}
+					customerProperties={customerProperties || this.handlecustomerProperties()}
+					initialValues={initialValues}
+					cartlayerBtnInitialized={cartlayerBtnInitialized}
+					onSubmit={this.handleFormSubmit}
 				/>
 			);
 		}
-
-		const {
-			checkoutInputClass = 'checkout-field',
-			checkoutButtonClass = 'checkout-button',
-			checkoutEditButtonClass = 'checkout-button-edit'
-		} = themeSettings;
-
-		return (
-			<Account
-				inputClassName={checkoutInputClass}
-				buttonClassName={checkoutButtonClass}
-				editButtonClassName={checkoutEditButtonClass}
-				settings={settings}
-				customerProperties={
-					customerProperties || this.handlecustomerProperties()
-				}
-				initialValues={initialValues}
-				cartlayerBtnInitialized={cartlayerBtnInitialized}
-				onSubmit={this.handleFormSubmit}
-			/>
-		);
 	}
 }
