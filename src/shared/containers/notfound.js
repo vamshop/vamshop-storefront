2f92833135002b91181cc432a5bc85331d3f105c
vam <vamshop@gmail.com>
2019-08-14 17:44:59 +0300

    Initial commit

---
+++ src/shared/containers/notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { NotFoundContainer } from 'theme';
+import { mapStateToProps, mapDispatchToProps } from '../containerProps';
+
+export default withRouter(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(NotFoundContainer)
+);
